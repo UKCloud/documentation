cc4ab5ed68c7445d5e2764e5ad4470bbcc1edd54
Oliver Nye <onye@SED00404.internal.local>
2018-07-13 17:30:30 +0100

    Fix dropdown issue

--- custom_template/styles/docfx.js
+++ custom_template/styles/docfx.js
@@ -391,7 +391,9 @@ $(function () {
               }
             }
             if (isActive) {
-              $(e).addClass(active);
+              if (window.location.pathname != '/index.html') {
+                $(e).addClass(active);
+              }
             }
           }
         });
