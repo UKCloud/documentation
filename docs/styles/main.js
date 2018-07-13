96e3f4f34398c538a9c1598fa47e351f4b7b2709
Chris Black <cblack@ukcloud.com>
2018-07-13 17:40:31 +0100

    Update generated documentation

--- docs/styles/main.js
+++ docs/styles/main.js
@@ -1,1 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+if (window.location.pathname === '/index.html') {
+  //Fix issues with random navs showing up on the homepage
+  if ($('div#breadcrumb').children().size() == 1) {
+    $('div#breadcrumb').remove();
+  }
+}
